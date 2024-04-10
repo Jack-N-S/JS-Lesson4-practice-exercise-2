64f56e623f62adb65dfdf70a306c3b353cb0e99f
Jack <155328654+Jack-N-S@users.noreply.github.com>
2024-04-10 00:03:10 -0400

    First commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var insta = document.querySelector(".fab.fa-instagram");
+insta.style.color = "#d45bb9";
+
+var facebook = document.querySelector(".fab.fa-facebook")
+facebook.style.color = "#6a99b5";
+
+var hours = document.querySelector(".contact p");
+hours.style.border = "1px solid #d45bb9";
+hours.style.padding = "1em";
